269bbe82f7f544011d5f6c17a8ba35b1ed983e1f
Caique Emanuel <caiqueemanuel142@gmail.com>
2026-01-05 21:24:04 -0300

    Aqui foi criado o novo projeto: um firewall bloqueador de ataque DoS e atualizado o README.md

---
+++ Firewall_Projects/firewall_block_Dos.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+from collections import defaultdict
+from scapy.all import sniff, IP
+
+#VARIÁVEL PARA LIMITE DE PACOTES POR SEGUNDO
+LIMITE = 40
+print(f"Definido limite de {LIMITE} pacotes por segundo.")
+
+#FUNÇÃO DE RETORNO PARA CONTAGEM DE IPs, CALCULO DE TAXA DE PACOTES E BLOQUEADOR DE IP
+def monitorar_pacotes(pkt):
+    origem_ip = pkt[IP].src
+    contador_pacotes[origem_ip] +=1
+
+    tempo_atual = time.time()
+    tempo_decorrido = tempo_atual - tempo_inicial[0]
+
+    if tempo_decorrido >= 1:
+        for ip, contador in contador_pacotes.items():
+            taxa_pacotes = contador / tempo_decorrido
+            print(f"IP: {ip} - Pacotes por segundo: {taxa_pacotes:.2f}")
+
+            if taxa_pacotes > LIMITE and ip not in ips_bloqueados:
+                print(f"Bloqueando IP {ip} por exceder o limite de pacotes.")
+                os.system(f"iptables -A INPUT -s {ip} -j DROP")
+                ips_bloqueados.add(ip)
+        
+        contador_pacotes.clear()
+        tempo_inicial[0] = tempo_atual
+    
+if __name__ == "__main__":
+    if os.getuid() != 0:
+        print("Este script precisa ser executado como root.")
+        sys.exit(1)
+    
+    contador_pacotes = defaultdict(int)
+    tempo_inicial = [time.time()]
+    ips_bloqueados = set()
+
+    print("Iniciando monitoramento de pacotes...")
+    sniff(filter="ip", prn=monitorar_pacotes)
